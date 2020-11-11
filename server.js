eaeefcbfa0a151c93578ab24089d7b13cda2855a
Shashi <shashitiwari1997@gmail.com>
2020-11-10 21:08:01 -0800

    lernov_repo

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.post("/messages", postRequest);
 //production
 
 if (process.env.NODE_ENV === "production" || process.env.NODE_ENV === "staging") {
-  app.use(express.static("/client/build"));
+  app.use(express.static("client/build"));
   // app.get("*", (req, res) => {
   //   res.sendFile(path.join(__dirname + "/client/build/index.html"));
   // });
