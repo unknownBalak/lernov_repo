11b332c4c38ee2cb3adfae8a30d5d5dc13c89839
Shashi <shashitiwari1997@gmail.com>
2020-11-11 01:04:00 -0800

    lernov repo

--- server.js
+++ server.js
@@ -29,17 +29,17 @@ mongoose.connect(
 //??
 
 //api routes
-app.get("/", getRequest);
+// app.get("/", getRequest);
 
 app.post("/messages", postRequest);
 
 //production
 
 if (process.env.NODE_ENV === "production" || process.env.NODE_ENV === "staging") {
-  app.use(express.static("client/build"));
-  // app.get("*", (req, res) => {
-  //   res.sendFile(path.join(__dirname + "/client/build/index.html"));
-  // });
+  app.use(express.static(path.join(__dirname, "build")));
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname + "build", "index.html"));
+  });
 }
 
 //listen
